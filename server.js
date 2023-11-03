253a429b715ad3e4b91f62b75ee1acbac6397ace
Wei Cheng Chen <DrChenWeiCheng@gmail.com>
2023-11-02 19:58:45 -0700

    phase:1

--- server.js
+++ server.js
@@ -3,8 +3,11 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   // Your code here
+  const indexPath = fs.readFileSync('./index.html')
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html')
+  res.end(indexPath)
 });
-
-const port = 5000;
+const port = 5001;
 
 server.listen(port, () => console.log('Server is listening on port', port));
